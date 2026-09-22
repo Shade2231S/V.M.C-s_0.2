d3d5a30163e0e5f6730107d6661137b8c9d63ac8
Shade2231S <matthewmcculloch149@gmail.com>
2026-09-22 15:14:50 -0300

    Initial commit

---
+++ V.M.C's_0.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V.M.C_s_0._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
